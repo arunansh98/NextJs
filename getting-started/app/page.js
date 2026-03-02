38e71afc7b1faf73a44d303219c3b9cb99c8a09a
Arunansh <arunansh98@gmail.com>
2026-03-02 17:53:33 +0530

    Initial Setup

---
+++ getting-started/app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  console.log('executing!');
+  
+  return (
+    <main>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <h1>Welcome to this NextJS Course!</h1>
+      <p>🔥 Let&apos;s get started! 🔥</p>
+    </main>
+  );
+}
+ 
