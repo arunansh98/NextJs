6818e1c81b8179e23e24f1f24da9b1d1a23ff8ae
Arunansh <arunansh98@gmail.com>
2026-03-03 11:19:24 +0530

    Header Component

--- getting-started/app/page.js
+++ getting-started/app/page.js
@@ -1,15 +1,16 @@
 import Link from "next/link";
+import Header from "@/components/header";
 
 export default function Home() {
-  console.log('executing!');
-  
+  console.log("executing!");
+
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
+      <Header />
       <p>🔥 Let&apos;s get started! 🔥</p>
-      <p><Link href="/about">About Us</Link></p>
+      <p>
+        <Link href="/about">About Us</Link>
+      </p>
     </main>
   );
 }
- 
