0903bcee76d6ad748a6d765b8851a1a65dc3d253
Arunansh <arunansh98@gmail.com>
2026-03-03 10:21:16 +0530

    About Us Page

--- getting-started/app/page.js
+++ getting-started/app/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   console.log('executing!');
   
@@ -6,6 +8,7 @@ export default function Home() {
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <p><Link href="/about">About Us</Link></p>
     </main>
   );
 }
