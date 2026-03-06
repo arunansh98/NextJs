c25e5d2fbeffed960bfba65ff5f3af18a9f7b1c2
Arunansh <arunansh98@gmail.com>
2026-03-06 14:08:58 +0530

    NavLink as Client Component

--- 05-onwards-foodies-starting-project/components/main-header/main-header.js
+++ 05-onwards-foodies-starting-project/components/main-header/main-header.js
@@ -3,27 +3,28 @@ import Logo from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header- background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
   return (
     <>
-    <MainHeaderBackground/>
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <Image src={Logo} alt="A plate with food on it" priority/>
-        NextLevel Food
-      </Link>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={Logo} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </>
   );
 }
