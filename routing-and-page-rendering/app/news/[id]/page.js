caace4b85b866c69dc29dead16689ba9b570c8cb
Arunansh <arunansh98@gmail.com>
2026-03-15 15:48:04 +0530

    Showing dynamic news list and news details

--- routing-and-page-rendering/app/news/[id]/page.js
+++
@@ -1,9 +0,0 @@
-export default async function NewsDetailPage({ params }) {
-  const { id } = await params;
-  return (
-    <>
-      <h1>News Details Page</h1>
-      <p>News ID: {id}</p>
-    </>
-  );
-}
