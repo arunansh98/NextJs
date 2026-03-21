4b45ed886ce8d541b6683b31ad5308b7c78da4d4
Arunansh <arunansh98@gmail.com>
2026-03-21 23:15:27 +0530

    Catch all routes and dealing with multiple/indefinite path segments

--- routing-and-page-rendering/app/archive/@archive/[year]/page.js
+++
@@ -1,8 +0,0 @@
-import NewsList from "@/components/news-list";
-import { getNewsForYear } from "@/lib/news";
-
-export default async function FilteredNewsPage({ params }) {
-  const newsYear = await params.year;
-  const news = getNewsForYear(newsYear);
-  return <NewsList news={news} />;
-}
