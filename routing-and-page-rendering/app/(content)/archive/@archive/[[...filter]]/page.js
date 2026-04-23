d141fb6691c82fa29b17d096af98571e68baad4a
Arunansh <arunansh98@gmail.com>
2026-04-23 14:51:39 +0530

    Showing Suspense Fallback for Filter Header

--- routing-and-page-rendering/app/(content)/archive/@archive/[[...filter]]/page.js
+++ routing-and-page-rendering/app/(content)/archive/@archive/[[...filter]]/page.js
@@ -8,6 +8,43 @@ import {
 import Link from "next/link";
 import { Suspense } from "react";
 
+async function FilterHeader({ year, month }) {
+  const availableYears = await getAvailableNewsYears();
+  let links = availableYears;
+
+  if (year && !month) {
+    links = getAvailableNewsMonths(year);
+  }
+
+  if (year && month) {
+    links = [];
+  }
+
+  if (
+    (year && !availableYears.includes(year)) ||
+    (month && !getAvailableNewsMonths(year).includes(month))
+  ) {
+    throw new Error("Invalid filter.");
+  }
+
+  return (
+    <header id="archive-header">
+      <nav>
+        <ul>
+          {links.map((link) => {
+            const href = year ? `/archive/${year}/${link}` : `/archive/${link}`;
+            return (
+              <li key={link}>
+                <Link href={href}>{link}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
 async function FilteredNews({ year, month }) {
   let news;
   if (year && !month) {
@@ -33,43 +70,11 @@ export default async function FilteredNewsPage({ params }) {
 
   const selectedMonth = filter?.[1];
 
-  const availableYears = await getAvailableNewsYears();
-  let links = availableYears;
-
-  if (selectedYear && !selectedMonth) {
-    links = getAvailableNewsMonths(selectedYear);
-  }
-
-  if (selectedYear && selectedMonth) {
-    links = [];
-  }
-
-  if (
-    (selectedYear && !availableYears.includes(selectedYear)) ||
-    (selectedMonth &&
-      !getAvailableNewsMonths(selectedYear).includes(selectedMonth))
-  ) {
-    throw new Error("Invalid filter.");
-  }
-
   return (
     <>
-      <header id="archive-header">
-        <nav>
-          <ul>
-            {links.map((link) => {
-              const href = selectedYear
-                ? `/archive/${selectedYear}/${link}`
-                : `/archive/${link}`;
-              return (
-                <li key={link}>
-                  <Link href={href}>{link}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </header>
+      <Suspense fallback={<p>Loading filter...</p>}>
+        <FilterHeader year={selectedYear} month={selectedMonth} />
+      </Suspense>
       <Suspense fallback={<p>Loading news ...</p>}>
         <FilteredNews year={selectedYear} month={selectedMonth} />
       </Suspense>
